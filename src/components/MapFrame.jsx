271a7d39d762f6c089c7fbf1f289d20166c949ea
Succi <P205934@surrey.ca>
2022-02-14 15:30:01 -0800

    changes

--- src/components/MapFrame.jsx
+++ src/components/MapFrame.jsx
@@ -3,28 +3,34 @@ import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import { countyService, statesService } from "../services/mapServices";
 
+const map = new Map({
+  basemap: "streets-vector",
+});
+
+const mapView = new MapView({
+  map,
+  zoom: 5,
+  center: [-97, 39],
+});
+
 const MapFrame = ({ layerList, setLayerList }) => {
+  const layerListRef = useRef(layerList);
   const mapRef = useRef(null);
 
-  const map = new Map({
-    basemap: "streets-vector",
-  });
-
-  const mapView = new MapView({
-    map,
-    zoom: 5,
-    center: [-97, 39],
-  });
-
   map.layers.on("change", (event) => {
-    const updatedLayerList = [...layerList, event.added[0].title];
-    setLayerList(updatedLayerList);
+    if (event.added[0].name) {
+      const updatedLayerList = [...layerListRef.current, event.added[0].name];
+      setLayerList(updatedLayerList);
+      console.log("added layer: " + event.added[0].name);
+    } else {
+      console.log("No name found for: " + event.added[0]);
+    }
   });
+
+  map.add(statesService);
 
   useEffect(() => {
     if (mapRef.current) {
-      map.add(countyService);
-      map.add(statesService);
       mapView.container = mapRef.current;
     }
   }, []);
